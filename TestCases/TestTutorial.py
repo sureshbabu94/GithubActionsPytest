8a83f5e00f4d553d1dfffa4653a3b13252c2f1f0
suresh <sureshbabu.rajamanickam@expleogroup.com>
2023-02-09 20:20:57 +0530

    options

--- TestCases/TestTutorial.py
+++ TestCases/TestTutorial.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import time
 
 def test_tutorial():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),chrome_options=options)
     driver.get("https://www.whatismybrowser.com/")
     driver.maximize_window()
     time.sleep(5)
