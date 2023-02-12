9ee0c611232d8bc9da98196d3320aff3b9d8a6ee
suresh <sureshbabu.rajamanickam@expleogroup.com>
2023-02-12 14:37:15 +0530

    options

--- TestCases/TestSeleniumGrid.py
+++ TestCases/TestSeleniumGrid.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 import time,os,shutil
+from jproperties import Properties
 
 def test_seleniumgrid():
-    # options = Options()
-    # options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')
+    configs = Properties()
+    with open('settings.properties', 'rb') as config_file:
+        configs.load(config_file)
+    if configs.get('BROWSER_NAME')=='firefox':
+        browser_options=FirefoxOptions()
+        browser_options.add_argument('ignore-certificate-errors')
+    else:
+        browser_options=ChromeOptions()
+        browser_options.add_argument('ignore-certificate-errors')
+
     desired_capabilities = {'browserName': "chrome", 'javascriptEnabled': True,'acceptInsecureCerts':True}
-    driver = webdriver.Remote(command_executor='https://localhost:4444/wd/hub',desired_capabilities=desired_capabilities)
+    driver = webdriver.Remote(command_executor='https://localhost:4444/wd/hub',options=browser_options)
     driver.get("https://www.whatismybrowser.com/")
     driver.maximize_window()
     time.sleep(5)
